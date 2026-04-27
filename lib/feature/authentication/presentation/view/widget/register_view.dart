6fc2667153985d368afa859dc7595196df9613dc
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2026-04-27 22:46:26 +0300

    Login activity

--- lib/feature/authentication/presentation/view/widget/register_view.dart
+++ lib/feature/authentication/presentation/view/widget/register_view.dart
@@ -4,7 +4,6 @@ import 'package:canzo_app/core/utils/const.dart';
 import 'package:canzo_app/feature/authentication/presentation/cubit/auth_cubit.dart';
 import 'package:canzo_app/feature/authentication/presentation/cubit/auth_state.dart';
 import 'package:canzo_app/feature/authentication/presentation/view/widget/custom_text_field.dart';
-import 'package:canzo_app/feature/authentication/presentation/view/widget/register_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
