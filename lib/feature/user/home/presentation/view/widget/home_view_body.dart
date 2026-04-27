ebc32dda3b3ae2d105e04b3e9031984633aa13f8
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2026-04-27 23:42:56 +0300

    Quick stats

--- lib/feature/user/home/presentation/view/widget/home_view_body.dart
+++ lib/feature/user/home/presentation/view/widget/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:canzo_app/core/utils/const.dart';
 import 'package:canzo_app/core/widget/custom_app_bar.dart';
 import 'package:canzo_app/feature/user/home/presentation/view/widget/custom_card_home.dart';
 import 'package:canzo_app/feature/user/home/presentation/view/widget/pickup_request.dart';
+import 'package:canzo_app/feature/user/home/presentation/view/widget/quick_stats_section.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -23,6 +24,8 @@ class HomeViewBody extends StatelessWidget {
             CustomCardHome(),
             sizeBox(),
             PickupRequestView(),
+            sizeBox(),
+            QuickStatsSection(),
           ],
         ),
       ),
---
+++ lib/feature/user/home/presentation/view/widget/quick_stats_section.dart
@@ -0,0 +1,73 @@
+import 'package:canzo_app/core/utils/color.dart';
+import 'package:canzo_app/core/utils/const.dart';
+import 'package:canzo_app/core/utils/style.dart';
+import 'package:flutter/material.dart';
+
+class QuickStatsSection extends StatelessWidget {
+  const QuickStatsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'QUICK STATS',
+          style: StyleText.style18.copyWith(
+            color: Colors.green.shade500,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        sizeBox(),
+        Row(
+          children: [
+            buildContainer('TOTALS', 4,'pickups'),
+            sizeBox(width: 10),
+            buildContainer('ACTIVE', 2, 'ongoing'),
+            sizeBox(width: 10),
+            buildContainer('DONE', 2, 'completed'),
+          ],
+        ),
+      ],
+    );
+  }
+  Widget buildContainer(String title, int price,String state) => Expanded(
+    child: Container(
+      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: StyleText.style13.copyWith(
+              color: Colors.black38,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          sizeBox(height: 10),
+          Text(
+            '$price',
+            style: StyleText.style26.copyWith(
+              color: AppColors.green,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          sizeBox(height: 10),
+          Text(
+            state,
+            style: StyleText.style13.copyWith(
+              color: Colors.black38,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+
+}
