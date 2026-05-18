3e559939b91e7be17a480f879e532054660d2ae6
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2026-05-19 01:51:50 +0300

    Fill baskets

--- lib/feature/user/home/presentation/view/widget/build_item_baskets.dart
+++ lib/feature/user/home/presentation/view/widget/build_item_baskets.dart
@@ -1,7 +1,5 @@
-import 'package:canzo_app/core/utils/app_strings.dart';
 import 'package:canzo_app/core/utils/style.dart';
 import 'package:canzo_app/feature/user/home/presentation/cubit/home_cubit.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
