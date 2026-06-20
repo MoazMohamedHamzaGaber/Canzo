93993e10952933981f89791444b527d1700d167d
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2026-06-20 21:25:18 +0300

    Change namespace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.canzo_app"
+    namespace = "com.portsaid.canzo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.canzo_app"
+        applicationId = "com.portsaid.canzo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
